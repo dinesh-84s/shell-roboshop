9d36839285a698acf27a5559520fbc3d5f1b239c
dineshkonda <dineshkonda3239@gmail.com>
2025-06-12 21:36:29 +0530

    colors

--- roboshop.sh
+++ roboshop.sh
@@ -13,8 +13,8 @@ do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type 
     t2.micro --security-group-ids sg-02ee33accb91fcb0a --tag-specifications "ResourceType=instance,
     Tags=[{Key=Name, Value=test}]" --query "Instances[0].InstanceId" --output text)
-
     if [ $instance != "frontend" ]
+    
     then
         IP=aws ec2 describe-instances --instance-ids $INSTANCE_ID -- query "Reservations[0].
         Instances[0].PrivateIpAddress" --output text
