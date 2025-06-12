151d80fafe4d61089b61a6ddc259aa88f5c00144
dineshkonda <dineshkonda3239@gmail.com>
2025-06-12 21:56:45 +0530

    colors

--- roboshop.sh
+++ roboshop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-02ee33accb91fcb0a" # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
