df707ca15b9adc1f23c312f2325b5606accbe8ac
dineshkonda <dineshkonda3239@gmail.com>
2025-06-19 20:20:34 +0530

    hello world started

--- catalogue.sh
+++ catalogue.sh
@@ -60,6 +60,8 @@ VALIDATE $? "Creating app directory"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading Catalogue"
 
+
+rm -rf /app/*
 cd /app
 unzip /tmp/catalogue.zip &>>$LOG_FILE
 VALIDATE $? "Unzipping the catalogue"
