57e9f066f86989c221b2bc1122904c3c5cf8ebea
dineshkonda <dineshkonda3239@gmail.com>
2025-06-19 16:33:52 +0530

    hello world started

---
+++ catalogue.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="/e[31m"
+G="/e[32m"
+Y="/e[33m"
+N="/e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "statred and execute the skript at with: $(date)" | tee -a $LOG_FILE
+
+#check users has root previlages or not 
+if [ $USERID -ne 0 ]
+then
+    echo -e $R "ERROR:: please run the script with using root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto to 127
+else 
+    echo "you are running with the root access" | tee -a $LOG_FILE
+fi
+
+#vaildate functions takes input as exit status what command they tried to install
+VALIDATE(){
+if [ $1 -eq 0 ]
+then
+    echo "Installing $2 is... $G SUCCESS $N" | tee -a $LOG_FILE
+else
+    echo "Installing $2 is... $R FAILURE $N" | tee -a $LOG_FILE
+    exit 1
+fi
+}
+
+dnf module disable nodejs -y $>>$LOG_FILE
+VALIDATE $? "Disabling the default nodejs"
+
+dnf module enable nodejs:20 -y $>>$LOG_FILE
+VALIDATE $? "Enabling the nodejs:20"
+
+dnf install nodejs -y $>>$LOG_FILE
+VALIDATE $? "Install the nodejs:20"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop $>>$LOG_FILE 
+VALIDATE $? "Creating roboshop system user"
+
+mkdir /app 
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip $>>$LOG_FILE
+VALIDATE $? "Downloading Catalogue"
+
+cd /app
+unzip /tmp/catalogue.zip $>>$LOG_FILE
+VALIDATE $? "Unzipping the catalogue"
+
+npm install $>>$LOG_FILE
+VALIDATE $? "Installing the dependencies"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service $>>$LOG_FILE
+VALIDATE $? "Copying catalouge services"
+
+systemctl daemon-reload $>>$LOG_FILE
+systemctl enable catalogue $>>$LOG_FILE
+systemctl start catalogue
+VALIDATE $? "Copying catalouge services"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y $>>$LOG_FILE
+VALIDATE $? "Installing mongodb client"
+
+mongosh --host mongodb.dineshkonda.site </app/db/master-data.js $>>$LOG_FILE
+
+
+
+
+
+
+
+
