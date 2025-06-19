5efc4917613a08fc67683a7a830959adb485a91a
dineshkonda <dineshkonda3239@gmail.com>
2025-06-19 12:42:29 +0530

    colors

---
+++ mongodb.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="/e[31m"
+G="/e[32m"
+Y="/e[33m"
+N="/e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "statred and execute the skript at with: $(date)" | tee -a $LOG_FILE
+
+#check users has root previlages or not 
+if [ $USERID -ne 0 ]
+then
+    echo -e $R "ERROR:: please run the script with using root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto to 127
+else 
+    echo "you are running with the root access" | tee -a $LOG_FILE
+fi
+
+#vaildate functions takes input as exit status what command they tried to install
+VALIDATE(){
+if [ $1 -eq 0 ]
+then
+    echo "Installing $2 is... $G SUCCESS $N" | tee -a $LOG_FILE
+else
+    echo "Installing $2 is... $R FAILURE $N" | tee -a $LOG_FILE
+    exit 1
+fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying mongodb.repo"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing mongodb server"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enabling mongodb repo"
+
+systemctl start mongod &>>$LOG_FILE
+VALIDATE $? "Start mongodb repo"
+
+sed -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf 
+VALIDATE $? "Editing mongodb file for remote connecton"
+
+systemctl restart mongod &>>$LOG_FILE
+VALIDATE $? "ReStart mongodb repo"
+
+
