7d7d9ccb82d1475d2032cae3e5c0dcb036e6ffd3
dineshkonda <dineshkonda3239@gmail.com>
2025-06-19 16:44:22 +0530

    hello world started

--- catalogue.sh
+++ catalogue.sh
@@ -34,44 +34,44 @@ else
 fi
 }
 
-dnf module disable nodejs -y $>>$LOG_FILE
+dnf module disable nodejs -y &>>$LOG_FILE
 VALIDATE $? "Disabling the default nodejs"
 
-dnf module enable nodejs:20 -y $>>$LOG_FILE
+dnf module enable nodejs:20 -y &>>$LOG_FILE
 VALIDATE $? "Enabling the nodejs:20"
 
-dnf install nodejs -y $>>$LOG_FILE
+dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Install the nodejs:20"
 
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop $>>$LOG_FILE 
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE 
 VALIDATE $? "Creating roboshop system user"
 
 mkdir /app 
 VALIDATE $? "Creating app directory"
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip $>>$LOG_FILE
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading Catalogue"
 
 cd /app
-unzip /tmp/catalogue.zip $>>$LOG_FILE
+unzip /tmp/catalogue.zip &>>$LOG_FILE
 VALIDATE $? "Unzipping the catalogue"
 
-npm install $>>$LOG_FILE
+npm install &>>$LOG_FILE
 VALIDATE $? "Installing the dependencies"
 
-cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service $>>$LOG_FILE
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
 VALIDATE $? "Copying catalouge services"
 
-systemctl daemon-reload $>>$LOG_FILE
-systemctl enable catalogue $>>$LOG_FILE
+systemctl daemon-reload &>>$LOG_FILE
+systemctl enable catalogue &>>$LOG_FILE
 systemctl start catalogue
 VALIDATE $? "Copying catalouge services"
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-mongosh -y $>>$LOG_FILE
+dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing mongodb client"
 
-mongosh --host mongodb.dineshkonda.site </app/db/master-data.js $>>$LOG_FILE
+mongosh --host mongodb.dineshkonda.site </app/db/master-data.js &>>$LOG_FILE
 
 
 
