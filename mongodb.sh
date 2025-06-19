2a74f63f3786b1b80818c81e11ab576f711b5f8a
dineshkonda <dineshkonda3239@gmail.com>
2025-06-19 12:57:20 +0530

    hello world started

--- mongodb.sh
+++ mongodb.sh
@@ -34,21 +34,21 @@ fi
 }
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? "Copying mongodb.repo"
+VALIDATE $? "Copying mongoDB.repo"
 
 dnf install mongodb-org -y &>>$LOG_FILE
-VALIDATE $? "Installing mongodb server"
+VALIDATE $? "Installing mongoDB"
 
 systemctl enable mongod &>>$LOG_FILE
-VALIDATE $? "Enabling mongodb repo"
+VALIDATE $? "Enabling mongoDB"
 
 systemctl start mongod &>>$LOG_FILE
-VALIDATE $? "Start mongodb repo"
+VALIDATE $? "Start mongoDB"
 
 sed -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf 
-VALIDATE $? "Editing mongodb file for remote connecton"
+VALIDATE $? "Editing mongoDB file for remote connections"
 
 systemctl restart mongod &>>$LOG_FILE
-VALIDATE $? "ReStart mongodb repo"
+VALIDATE $? "ReStart mongoDB repo"
 
 
