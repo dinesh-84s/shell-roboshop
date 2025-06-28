89740ac46489cf34242103938995c4330c16b4d3
dineshkonda <dineshkonda3239@gmail.com>
2025-06-28 21:35:05 +0530

    colors

--- redis.sh
+++ redis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+
 START_TIME=$(date +%S)
 USERID=$(id -u)
-
 R="/e[31m"
 G="/e[32m"
 Y="/e[33m"
@@ -11,12 +11,12 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
-echo "statred and execute the skript at with: $(date)" | tee -a $LOG_FILE
+echo "started and execute the script at with: $(date)" | tee -a $LOG_FILE
 
-#check users has root previlages or not 
+# check users has root privileges or not 
 if [ $USERID -ne 0 ]
 then
-    echo -e $R "ERROR:: please run the script with using root access $N" | tee -a $LOG_FILE
+    echo -e "$R ERROR:: please run the script with using root access $N" | tee -a $LOG_FILE
     exit 1 #give other than 0 upto to 127
 else 
     echo "you are running with the root access" | tee -a $LOG_FILE
@@ -43,14 +43,14 @@ dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing redis"
 
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -i '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "Edited redis.conf to accept remote connection"
 
 systemctl enable redis &>>$LOG_FILE
 VALIDATE $? "Enabling the redis"
 
 systemctl start redis &>>$LOG_FILE
-VALIDATE $? "start the redis"
+VALIDATE $? "start the Redis"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
