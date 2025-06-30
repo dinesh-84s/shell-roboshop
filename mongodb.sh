65086fdf78869e7e0d304a5f020a095843969907
dineshkonda <dineshkonda3239@gmail.com>
2025-06-30 17:53:50 +0530

    hello-worls-started

--- mongodb.sh
+++ mongodb.sh
@@ -49,4 +49,4 @@ sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Editing mongoDB file for remote connections"
 
 systemctl restart mongod &>>$LOG_FILE
-VALIDATE $? "ReStart mongoDB repo"
+VALIDATE $? "Restart mongoDB repo"
