83ef06d4614ed5d3ac2988fe411e47ad6c233c4a
dineshkonda <dineshkonda3239@gmail.com>
2025-06-30 17:22:43 +0530

    hello-worls-started

--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -8,8 +9,10 @@ LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
+
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
 # check the user has root priveleges or not
 if [ $USERID -ne 0 ]
 then
@@ -18,6 +21,7 @@ then
 else
     echo "You are running with root access" | tee -a $LOG_FILE
 fi
+
 # validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
@@ -28,25 +32,36 @@ VALIDATE(){
         exit 1
     fi
 }
+
 dnf module disable nginx -y &>>$LOG_FILE
 VALIDATE $? "Disabling Default Nginx"
+
 dnf module enable nginx:1.24 -y &>>$LOG_FILE
 VALIDATE $? "Enabling Nginx:1.24"
+
 dnf install nginx -y &>>$LOG_FILE
 VALIDATE $? "Installing Nginx"
+
 systemctl enable nginx  &>>$LOG_FILE
-systemctl start nginx
+systemctl start nginx 
 VALIDATE $? "Starting Nginx"
+
 rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
 VALIDATE $? "Removing default content"
+
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading frontend"
-cd /usr/share/nginx/html
+
+cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "unzipping frontend"
+
 rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
 VALIDATE $? "Remove default nginx conf"
+
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
 VALIDATE $? "Copying nginx.conf"
-systemctl restart nginx
+
+systemctl restart nginx 
 VALIDATE $? "Restarting nginx"
+
