964e2b9bed7b31595bc78abb345680fabec698c4
dineshkonda <dineshkonda3239@gmail.com>
2025-07-01 17:44:31 +0530

    hello-worls-started

--- shipping.sh
+++ shipping.sh
@@ -83,9 +83,9 @@ VALIDATE $? "start shipping"
 dnf install mysql -y  &>>$LOG_FILE
 VALIDATE $? "install mysql"
 
-mysql -h mysql.dineshkonda.site -uroot -pRoboShop@1 < /app/db/schema.sql  &>>$LOG_FILE
-mysql -h mysql.dineshkonda.site -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOG_FILE
-mysql -h mysql.dineshkonda.site -uroot -pRoboShop@1 < /app/db/master-data.sql  &>>$LOG_FILE
+mysql -h mysql.dineshkonda.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql  &>>$LOG_FILE
+mysql -h mysql.dineshkonda.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql  &>>$LOG_FILE
+mysql -h mysql.dineshkonda.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql  &>>$LOG_FILE
 VALIDATE $? "Loading data into mysql"
 
 systemctl restart shipping  &>>$LOG_FILE
