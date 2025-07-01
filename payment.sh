155c41d4560db27eb86597c6df78c06c784e535a
dineshkonda <dineshkonda3239@gmail.com>
2025-07-02 00:23:40 +0530

    hello-worls-started

--- payment.sh
+++ payment.sh
@@ -54,7 +54,6 @@ VALIDATE $? "Creating app directory"
 curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading payment"
 
-
 rm -rf /app/*
 cd /app
 unzip /tmp/payment.zip &>>$LOG_FILE
