6b1f2e0581461ce11cd21e600e8cae1b89420abd
dineshkonda <dineshkonda3239@gmail.com>
2025-07-02 19:00:43 +0530

    hello-worls-started

--- mysql.sh
+++ mysql.sh
@@ -1,54 +1,53 @@
 #!/bin/bash
 
-START_TIME=$(date +%S)
+START_TIME=$(date +%s)
 USERID=$(id -u)
-R="/e[31m"
-G="/e[32m"
-Y="/e[33m"
-N="/e[0m"
-LOGS_FOLDER="/var/log/shellscript-logs"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
-echo "statred and execute the skript at with: $(date)" | tee -a $LOG_FILE
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-#check users has root previlages or not 
+# check the user has root priveleges or not
 if [ $USERID -ne 0 ]
 then
-    echo -e $R "ERROR:: please run the script with using root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto to 127
-else 
-    echo "you are running with the root access" | tee -a $LOG_FILE
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-echo "please enter root password to setup"
+echo "Please enter root password to setup"
 read -s MYSQL_ROOT_PASSWORD
 
-#vaildate functions takes input as exit status what command they tried to install
+# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
-        echo "Installing $2 is... $G SUCCESS $N" | tee -a $LOG_FILE
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
     else
-        echo "Installing $2 is... $R FAILURE $N" | tee -a $LOG_FILE
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
-fi
+    fi
 }
 
 dnf install mysql-server -y &>>$LOG_FILE
-VALIDATE $? "Installing mysql"
+VALIDATE $? "Installing MySQL server"
 
 systemctl enable mysqld &>>$LOG_FILE
-VALIDATE $? "Enabling mysql"
+VALIDATE $? "Enabling MySQL"
 
-systemctl start mysqld  &>>$LOG_FILE
-VALIDATE $? "Start mysql"
+systemctl start mysqld   &>>$LOG_FILE
+VALIDATE $? "Starting MySQL"
 
-mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD  &>>$LOG_FILE
-VALIDATE $? "setting mysql root password"
-
+mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD &>>$LOG_FILE
+VALIDATE $? "Setting MySQL root password"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
