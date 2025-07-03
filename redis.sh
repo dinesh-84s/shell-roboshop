4612c062ffd26d97c67002f4749e5ec036096d21
dineshkonda <dineshkonda3239@gmail.com>
2025-07-03 17:21:43 +0530

    hello-worls-started

--- redis.sh
+++ redis.sh
@@ -1,56 +1,55 @@
 #!/bin/bash
 
-START_TIME=$(date +%S)
+START_TIME=$(date +%s)
 USERID=$(id -u)
-R="/e[31m"
-G="/e[32m"
-Y="/e[33m"
-N="/e[0m"
-LOGS_FOLDER="/var/log/shellscript-logs"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
-echo "started and execute the script at with: $(date)" | tee -a $LOG_FILE
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-# check users has root privileges or not 
+# check the user has root priveleges or not
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R ERROR:: please run the script with using root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto to 127
-else 
-    echo "you are running with the root access" | tee -a $LOG_FILE
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-#vaildate functions takes input as exit status what command they tried to install
+# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
-if [ $1 -eq 0 ]
-then
-    echo "Installing $2 is... $G SUCCESS $N" | tee -a $LOG_FILE
-else
-    echo "Installing $2 is... $R FAILURE $N" | tee -a $LOG_FILE
-    exit 1
-fi
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
 }
 
 dnf module disable redis -y &>>$LOG_FILE
-VALIDATE $? "Disabling redis"
+VALIDATE $? "Disabling Default Redis version"
 
 dnf module enable redis:7 -y &>>$LOG_FILE
-VALIDATE $? "Enabling redis:7"
+VALIDATE $? "Enabling Redis:7"
 
 dnf install redis -y &>>$LOG_FILE
-VALIDATE $? "Installing redis"
-
+VALIDATE $? "Installing Redis"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-VALIDATE $? "Edited redis.conf to accept remote connection"
+VALIDATE $? "Edited redis.conf to accept remote connections"
 
 systemctl enable redis &>>$LOG_FILE
-VALIDATE $? "Enabling the redis"
+VALIDATE $? "Enabling Redis"
 
-systemctl start redis &>>$LOG_FILE
-VALIDATE $? "start the Redis"
+systemctl start redis  &>>$LOG_FILE
+VALIDATE $? "Started Redis"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
