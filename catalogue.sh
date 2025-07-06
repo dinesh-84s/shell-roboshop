3236ec40536edd90f187ad582573495416bda63a
dineshkonda <dineshkonda3239@gmail.com>
2025-07-06 20:25:59 +0530

    hello-worls-started

--- catalogue.sh
+++ catalogue.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-source=./common.sh
+source ./common.sh
 app_name=catalogue
 
 check_root
 app_setup
 nodejs_setup
-
-
+systemd_setup
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
-STATUS=$(mongosh --host mongodb.dineshkonda.site.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+STATUS=$(mongosh --host mongodb.daws84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $STATUS -lt 0 ]
 then
-    mongosh --host mongodb.dineshkonda.site </app/db/master-data.js &>>$LOG_FILE
+    mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
 else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
--- common.sh
+++ common.sh
@@ -14,28 +14,26 @@ mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 app_setup(){
-    id roboshop &>>$LOG_FILE
+        id roboshop &>>$LOG_FILE
     if [ $? -ne 0 ]
-then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-    VALIDATE $? "Creating roboshop system user"
-else
-    echo -e "System user roboshop already created ... $Y SKIPPING $N"
-fi
+    then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+        VALIDATE $? "Creating roboshop system user"
+    else
+        echo -e "System user roboshop already created ... $Y SKIPPING $N"
+    fi
 
-mkdir -p /app 
-VALIDATE $? "Creating app directory"
+    mkdir -p /app 
+    VALIDATE $? "Creating app directory"
 
-curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading Catalogue"
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOG_FILE
+    VALIDATE $? "Downloading $app_name"
 
-rm -rf /app/*
-cd /app 
-unzip /tmp/$app_name.zip &>>$LOG_FILE
-VALIDATE $? "unzipping $app_name"
+    rm -rf /app/*
+    cd /app 
+    unzip /tmp/$app_name.zip &>>$LOG_FILE
+    VALIDATE $? "unzipping $app_name"
 }
-
-
 
 nodejs_setup(){ 
 dnf module disable nodejs -y &>>$LOG_FILE
@@ -51,7 +49,7 @@ npm install &>>$LOG_FILE
 VALIDATE $? "Installing Dependencies"
 }
 
-systemmd(){
+systemd_setup(){
     cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
     VALIDATE $? "Copying app_name service"
 
@@ -70,6 +68,7 @@ else
     echo "You are running with root access" | tee -a $LOG_FILE
 fi
 }
+
 # validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
